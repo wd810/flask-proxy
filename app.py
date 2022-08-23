07b0d5e70fcb4d274152a761db19ee66b243db11
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-08-22 21:36:15 -0300

    enable cors

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from cgitb import text
+from crypt import methods
 from flask import Flask, request
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 app.debug = True
 
-@app.route("/<url>")
-def home(url):
-    data = request.json
-    lch = requests.get(data['url'])
-    print(lch.json())
-    return lch.json()
+@app.route("/", methods=['GET'])
+def home():
+    url = request.args['url']
+    data = requests.get(url)
+    print(data.json())
+    return data.json()
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
